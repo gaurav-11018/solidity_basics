49876657c00aad2834f659ed71df5243b36277ed
gaurav-11018 <79459872+gaurav-11018@users.noreply.github.com>
2022-03-28 23:26:29 +0530

    Add files via upload

---
+++ storage.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.6;
+
+
+contract Storage{
+uint256  storenum;
+
+
+function set(uint256 x) public{
+   storenum=x;
+}
+
+function get() view public returns(uint){
+   return storenum;
+}
+
+
+}
